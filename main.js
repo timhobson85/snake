d8428599c54a5fa306acfe65df49bd154b6baed1
timhobson85 <claehobson@gmail.com>
2020-04-18 22:35:38 +1000

    core game created

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 const canvas = document.getElementById('snake')
 const ctx = canvas.getContext('2d');
 
-const pixelSize = 10;
-canvas.width = 200;
-canvas.height = 200;
+canvas.width = 400;
+canvas.height = 400;
 
-ctx.fillStyle = 'black';
-ctx.fillRect( 0, 0, 800, 800 )
+const frameRate = 10;
+const screenSize = 10;
+const pixelSize = canvas.width / screenSize;
 
-// let pos, vel, food, snake;
-
-// draw a 'pixel'
-// ctx.fillStyle = 'fuchsia';
-// ctx.fillRect( canvas.width / 2 - ( pixelSize  ) , canvas.height / 2 - ( pixelSize  ), pixelSize, pixelSize )
-
-createScreenArray = () => {
-  const screen = new Array( canvas.height / pixelSize ).fill(null)
-    .map( () => new Array( canvas.width / pixelSize ).fill(0))
-  // console.log(screen);
-}
-
-createScreenArray();
-
-let snake;
+let pos, vel, food, snake;
 
 init = () => {
   pos = { x: 10, y: 10 };
@@ -34,25 +20,84 @@ init = () => {
     { x: 10, y: 10 }
   ];
 
-  // placeFood();
+  placeFood();
 }
 
 init();
 
-function gameLoop() {
-  for(let cell of snake){
-    // console.log( cell )
-    ctx.fillStyle = 'fuchsia';
-    ctx.fillRect( cell.x * pixelSize , cell.y * pixelSize , pixelSize, pixelSize )
+function placeFood() {
+  food = {
+    x: Math.floor(Math.random() * pixelSize),
+    y: Math.floor(Math.random() * pixelSize ),
+  }
+
+  for (let cell of snake) {
+   if (cell.x === food.x && food.y === cell.y) {
+     return placeFood();     
+   } 
   }
 }
-gameLoop();
 
-// init();
+document.addEventListener('keydown', keydown);
 
-// function placeFood() {
-//   food = {
-//     x: Math.floor(Math.random() * ),
-//     y: Math.floor(Math.random() * ),
-//   }
-// }
+function keydown(e) {
+  switch(e.keyCode) {
+    case 37: {
+      return vel = { x: -1, y: 0 }
+    }
+    case 38: {
+      return vel = { x: 0, y: -1 }
+    }
+    case 39: {
+      return vel = { x: 1, y: 0 }
+    }
+    case 40: {
+      return vel = { x: 0, y: 1 }
+    }
+  }
+}
+
+setInterval(() => {
+  requestAnimationFrame(gameLoop)
+}, 1000 / frameRate);
+
+function gameLoop() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect( 0, 0, canvas.width, canvas.height )
+  
+  ctx.fillStyle = 'fuchsia';
+  for(let cell of snake){
+    for (let cell of snake) {
+      ctx.fillRect( cell.x*screenSize, cell.y*screenSize, screenSize, screenSize)
+    }
+  }
+
+  ctx.fillStyle = 'cyan';
+  ctx.fillRect(food.x*screenSize, food.y*screenSize, screenSize, screenSize);
+
+  pos.x += vel.x;
+  pos.y += vel.y;
+
+  if (pos.x < 0 || pos.x > pixelSize || pos.y < 0 || pos.y > pixelSize) {
+    init();    
+  }
+
+  if (food.x === pos.x && food.y === pos.y) {
+    snake.push({...pos});
+    pos.x += vel.x;
+    pos.y += vel.y;
+    placeFood();
+  } 
+
+  if (vel.x || vel.y) {
+    for ( let cell of snake ) {
+      if (cell.x === pos.x && cell.y === pos.y) {
+        return init();        
+      }
+    }
+    snake.push({...pos});
+    snake.shift();
+  }
+  
+}
+
